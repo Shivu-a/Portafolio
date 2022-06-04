ecd3acca2c76cf68968deb7734896c0975905af6
Shivu-a <sebaingame@gmail.com>
2022-06-04 15:20:05 -0300

    Agregado boton de light/dark mode

---
+++ scripts/modeSwitch.js
@@ -0,0 +1,48 @@
+let body = document.querySelector("body");
+let botonSwitch = document.querySelector("#botonSwitch");
+let h3 = document.querySelectorAll("h3");
+let curso = document.querySelectorAll(".curso");
+let proyecto = document.querySelectorAll(".proyecto");
+let espanol = document.querySelectorAll(".espanol");
+let ingles = document.querySelectorAll(".ingles");
+let formContacto = document.querySelectorAll(".formContacto");
+
+const arrayDivs = [curso, proyecto, espanol, ingles, formContacto];
+
+function cambiarFondo() {
+  if (body.className === "dark-mode") {
+    body.style.backgroundColor = "#dee4e7";
+    body.style.color = "black";
+
+    h3.forEach((element) => {
+      element.style.color = "black";
+    });
+
+    for (let i = 0; i < arrayDivs.length; i++) {
+      arrayDivs[i].forEach((element) => {
+        element.id = "borderBoxWhite";
+      });
+    }
+
+    body.className = "light-mode";
+  } else {
+    body.style.backgroundColor = "#1e1e1e";
+    body.style.color = "white";
+
+    h3.forEach((element) => {
+      element.style.color = "white";
+    });
+
+    for (let i = 0; i < arrayDivs.length; i++) {
+      arrayDivs[i].forEach((element) => {
+        element.id = "";
+      });
+    }
+
+    body.className = "dark-mode";
+  }
+}
+
+botonSwitch.addEventListener("click", () => {
+  cambiarFondo();
+});
